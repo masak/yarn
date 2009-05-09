4509a390d0a75ef6299cfc82bc80ab688e8a04a4
Carl Masak <cmasak@gmail.com>
2009-05-09 18:30:19 +0200

    [Yarn] halfway to getting POSTing to work

--- lib/Yarn.pm
+++ lib/Yarn.pm
@@ -8,29 +8,50 @@ use Tags;
 class Yarn {
     method call($env) {
         my Web::Request $req .= new($env);
-        
         my Web::Response $res .= new;
 
-        my @posts =
-            { title => "foo", content => "foo content" },
-            { title => "bar", content => "bar content" };
+        if $req.request_method eq 'POST' {
+            my $fh = open('data/posts', :w) or die $!;
+            $fh.print( [ { title => 'test', content => '1, 2, 3' } ].perl );
+            $fh.close;
+        }
 
-        $res.write(show {
-            html {
-                head { title { 'Yarn' } }
-                body {
-                    p {
-                        a :href</create>, { 'Create new post' }
-                    }
-                    for @posts -> $post {
-                        div :class<post>, {
-                            h1 { $post<title> };
-                            div { $post<content> };
+        given $req.GET<mode> // '' {
+            when 'write' {
+                $res.write(show {
+                    html { title { 'Writing a post' } }
+                    body {
+                        form :action</>, :method<post>, {
+                            input :type<submit>, {};
                         }
                     }
-                }
+                });
             }
-        });
+
+            default {
+                my @posts =
+                    { title => "foo", content => "foo content" },
+                    { title => "bar", content => "bar content" };
+
+                $res.write(show {
+                    html {
+                        head { title { 'Yarn' } }
+                        body {
+                            p {
+                                a :href</?mode=write>, { 'Write a new post' }
+                            }
+                            for @posts -> $post {
+                                div :class<post>, {
+                                    h1 { $post<title> };
+                                    div { $post<content> };
+                                }
+                            }
+                        }
+                    }
+                });
+            }
+        }
+
         $res.finish();
     }
 }
