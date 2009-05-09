ef1a111cd9d92d0ae229952455c6eaa7802e0d87
Carl Masak <cmasak@gmail.com>
2009-05-09 18:00:35 +0200

    [Yarn] now outputs posts

--- lib/Yarn.pm
+++ lib/Yarn.pm
@@ -3,12 +3,34 @@ use v6;
 use Web::Request;
 use Web::Response;
 
+use Tags;
+
 class Yarn {
     method call($env) {
         my Web::Request $req .= new($env);
         
         my Web::Response $res .= new;
-        $res.write('<html><p>OH HAI</p></html>');
+
+        my @posts =
+            { title => "foo", content => "foo content" },
+            { title => "bar", content => "bar content" };
+
+        $res.write(show {
+            html {
+                head { title { 'Yarn' } }
+                body {
+                    p {
+                        a :href</create>, { 'Create new post' }
+                    }
+                    for @posts -> $post {
+                        div :class<post>, {
+                            h1 { $post<title> };
+                            div { $post<content> };
+                        }
+                    }
+                }
+            }
+        });
         $res.finish();
     }
 }
